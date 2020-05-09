5d00fc0bc96220bedc52838013ad774a4a30cd8a
Soham Roy <sohamroy@sohamroy.me>
2020-05-09 16:25:14 -0500

    Init commit

---
+++ src/test/java/me/sohamroy/trendmicroservices/lab/random/RandomApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sohamroy.trendmicroservices.lab.random;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
